47d7b89c2a04600327adf2e11281ea09ed011240
Randy <rloiseau0@gmail.com>
2024-12-13 19:23:35 +0100

    Encapsulate global variables into classes and improve code organization

--- script.js
+++ script.js
@@ -1,141 +1,126 @@
 "use strict";
 
 const addButtonElement = document.getElementById('counterAdd');
-const subButtonElement = document.getElementById('counterSub');
-const counterDisplayElement = document.getElementById('counterDisplay');
-const counterAutoElement = document.getElementById('counterAuto');
-const errorMessageElement = document.getElementById('errorMessage');
-const improveClickElement = document.getElementById('clickImprove');
 
-const mielCanvas = document.getElementById('newElementsContainer');
-const miel = new Image();
-const boite = new Image();
-miel.src = "Miel.png";
-boite.src = "Boite.png";
-mielCanvas.width = window.innerWidth;
-mielCanvas.height = window.innerHeight;
+
 
 class Game {
-    constructor() {
-        this.total = 0;
-        this.autoInterval = null;
-        this.Doigby = new Audio('youtube_8P5WCI0iQlo_audio.mp3');
-        this.unlockStatus = 0;
-        this.currentSpeed = 1;
-        this.milestonesMap = new Map([
-            [0, { img: miel, factor: 1, size: 32 }],
-            [200, { img: boite, factor: 200, size: 128 }]
-        ]);}
+    static total = 100000;
+    static unlockStatus = 0;
+    static currentSpeed = 1;
+    static spaceCanvas = document.getElementById('newElementsContainer');
+    static autoInterval = null;
+    static Doigby = new Audio('youtube_8P5WCI0iQlo_audio.mp3');
+    static counterAutoElement = document.getElementById('counterAuto');
+    static counterDisplayElement = document.getElementById('counterDisplay');
+    static milestonesMap = new Map([
+        [0, { img: Object.assign(new Image(), { src: "Miel.png" }), factor: 1, size: 32 }],
+        [2000, { img: Object.assign(new Image(), { src: "Boite.png" }), factor: 2000, size: 128 }]
+    ]);
 
-        updateImage() {
-            let currentConfig = null
-            for (const [limit, config]  of this.milestonesMap) {
-                    if (this.total>=limit) {    
-                        currentConfig = config
-                    }
+    static updateImage() {
+        let currentConfig = null;
+        for (const [limit, config] of this.milestonesMap) {
+            if (this.total >= limit) {
+                currentConfig = config;
             }
-            return currentConfig;
         }
+        return currentConfig;
+    }
+
+    static refresh() {
+        let config = this.updateImage();
+        const ctx = this.spaceCanvas.getContext("2d");
+        ctx.clearRect(0, 0, this.spaceCanvas.width, this.spaceCanvas.height);
+        const { img, factor, size } = config;
+        this.counterDisplayElement.textContent = this.total;
+        for (let i = 0; i < Math.floor(this.total / factor); i++) {
+            ctx.drawImage(img, (i * size) % this.spaceCanvas.width, Math.floor((i * size) / this.spaceCanvas.width) * size, size, size);
+        }
+    }
+    static onResize() {
+        console.log("La fenêtre est redimensionnée !");
+        console.log("Canvas :", this.spaceCanvas.width);
+        this.spaceCanvas.width = window.innerWidth;
+        this.spaceCanvas.height = window.innerHeight;
+        this.refresh();
+    }
+
+    static onClickCounter() {
+        if (this.total < 100 && this.unlockStatus === 0) {
+            ErrorManager.errorMessageDisplay(); }
+        else if(this.total>=100 && this.unlockStatus === 0) {
+            this.unlockStatus = 1
+            this.counterAutoElement.textContent =`Current speed : ${this.currentSpeed}/s`
+            // this.autoInterval = setInterval(Game.incrementCounter(), 1000);
+            this.autoInterval = setInterval(() => Game.incrementCounter(), 1000);
+            this.total -=100;
+            this.refresh();
         }
     
+    }
+
+    static incrementCounter() {
+        
+        this.total += ClickImprovement.clickIncrement;
+        this.refresh();
+        console.log(this.total);
+    }
+
+    static onMouseDown() {
+     this.total += ClickImprovement.clickIncrement;
+    let audioPlay = new Audio('PLOUF.mp3');
+    audioPlay.play();
+    this.Doigby.play();
+    this.refresh();
+    }
+}
 
 
 class ClickImprovement {
-    constructor() {
-        this.clickIncrement = 1;
-        this.milestones = [10, 50, 100, 250, 500, 1000];
-        this.currentMilestoneIndex = 0;
+
+    static clickIncrement = 1;
+    static milestones = [10, 50, 100, 250, 500, 1000,20000,50000,99999999999];
+    static currentMilestoneIndex = 0;
+    static improveClickElement = document.getElementById('clickImprove');
+    static initialize() {
+        this.actualMilestone();
+        Game.onResize();
+    }
+    static actualMilestone() {
+        this.improveClickElement.textContent = `Improve Click (${this.milestones[this.currentMilestoneIndex]})`;
     }
 
-    actualMilestone() {
-        improveClickElement.textContent = `Improve Click (${this.milestones[this.currentMilestoneIndex]})`;
-    }
-
-    upgradeClick() {
-        if (game.total >= this.milestones[this.currentMilestoneIndex]) {
+    static upgradeClick() {
+        if (Game.total >= this.milestones[this.currentMilestoneIndex]) {
             this.clickIncrement = Math.pow(2, this.currentMilestoneIndex + 1);
             this.currentMilestoneIndex++;
             console.log(this.milestones[this.currentMilestoneIndex - 1]);
-            game.total -= this.milestones[this.currentMilestoneIndex - 1];
-            updateCounterDisplay();
+            Game.total -= this.milestones[this.currentMilestoneIndex - 1];
             this.actualMilestone();
-            refresh();
+            Game.refresh();
         } else {
-            errorMessageDisplay();
+            ErrorManager.errorMessageDisplay();
         }
     }
 }
 
-const updateCounterDisplay = () => {
-    if (game.total < 0) {
-        game.total = 0;
+class ErrorManager {
+    static errorMessageElement = document.getElementById('errorMessage');
+    static errorMessageDisplay() {
+        this.errorMessageElement.style.animation = 'none';
+        this.errorMessageElement.classList.add('visible');
+        setTimeout(() => {
+            this.errorMessageElement.style.animation = 'fadeOut 1s forwards';
+        }, 1000);
     }
-    document.body.style.setProperty('background-color', 'white');
-    counterDisplayElement.textContent = game.total;
-};
-
-addEventListener("resize", () => {
-    mielCanvas.width = window.innerWidth;
-    mielCanvas.height = window.innerHeight;
-    refresh();
-});
-
-const refresh = () => {
-    let config = game.updateImage()
-    const ctx = mielCanvas.getContext("2d");
-    ctx.clearRect(0, 0, mielCanvas.width, mielCanvas.height);
-    const {img, factor, size} = config;
-    for (let i = 0; i < Math.floor((game.total)/factor); i++) {
-            ctx.drawImage(img, (i * size) % mielCanvas.width, Math.floor((i * size) / mielCanvas.width) * size, size, size);
-    
-    } }
-;
-
-addButtonElement.addEventListener('mousedown', () => {
-    game.total += clickImprovement.clickIncrement;
-    let audioPlay = new Audio('PLOUF.mp3');
-    audioPlay.play();
-    updateCounterDisplay();
-    game.Doigby.play();
-    refresh();
-});
-
-counterAutoElement.addEventListener('click', () => {
-     let incrementCounter = () => {
-            game.total += clickImprovement.clickIncrement;
-            refresh();
-            console.log(game.total);
-            updateCounterDisplay();
-            
-        };
-    if (game.total < 100 && game.unlockStatus === 0) {
-        errorMessageDisplay(); }
-    else if(game.total>=100 && game.unlockStatus === 0) {
-        game.unlockStatus = 1
-        counterAutoElement.textContent =`Current speed : ${game.currentSpeed}/s`
-        game.autoInterval = setInterval(incrementCounter, 1000);
-
-    }
-    
-
-        
-    }
-);
-
-improveClickElement.addEventListener('click', () => {
-    clickImprovement.upgradeClick();
-});
-
-const errorMessageDisplay = () => {
-    errorMessageElement.style.animation = 'none';
-    errorMessageElement.classList.add('visible');
-    setTimeout(() => {
-        errorMessageElement.style.animation = 'fadeOut 1s forwards';
-    }, 1000);
 }
 
-const game = new Game();
-const clickImprovement = new ClickImprovement();
 
-clickImprovement.actualMilestone();
-updateCounterDisplay();
+
+
+addButtonElement.addEventListener('mousedown', () => Game.onMouseDown())
+Game.counterAutoElement.addEventListener('click', () => Game.onClickCounter())
+ClickImprovement.improveClickElement.addEventListener('click', () => ClickImprovement.upgradeClick())
+ClickImprovement.initialize();
+window.addEventListener("resize", () => Game.onResize() );
